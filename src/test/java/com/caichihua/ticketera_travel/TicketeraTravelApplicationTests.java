9c26b8cd963166170217fdfc0fef31fea1721a39
Larry Caichihua Vergara <lcaichihua@gmail.com>
2024-02-11 22:45:58 -0500

    first commit

---
+++ src/test/java/com/caichihua/ticketera_travel/TicketeraTravelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caichihua.ticketera_travel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketeraTravelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
