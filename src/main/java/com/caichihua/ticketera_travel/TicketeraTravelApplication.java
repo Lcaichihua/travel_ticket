c5cfce357a651c042621a05a27a002cd088a7225
Larry Caichihua Vergara <lcaichihua@gmail.com>
2024-03-07 22:05:12 -0500

    test repositories

--- src/main/java/com/caichihua/ticketera_travel/TicketeraTravelApplication.java
+++ src/main/java/com/caichihua/ticketera_travel/TicketeraTravelApplication.java
@@ -2,12 +2,14 @@ package com.caichihua.ticketera_travel;
 
 import com.caichihua.ticketera_travel.domain.repositories.FlyRepository;
 import com.caichihua.ticketera_travel.domain.repositories.HotelRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class TicketeraTravelApplication implements CommandLineRunner {
 
 
@@ -24,5 +26,6 @@ public class TicketeraTravelApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var fly = flyRepository.findById(15L).get();
 		var hotel =hotelRepository.findById(7L).get();
+		log.info(String.valueOf(fly));
 	}
 }
